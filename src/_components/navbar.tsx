7cd56b06dc701a5e5449b41f6c02da077c9dfff2
hafezfhmi <hafezfahmi2000@gmail.com>
2024-10-08 23:31:18 +0800

    feat: add nav bar

---
+++ src/_components/navbar.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Navbar() {
+  return (
+    <nav className="flex justify-between items-center pt-[30px]">
+      <Image src="/Logo.svg" alt="Positivus home" width={144} height={24} />
+      <button className="flex items-center w-6 h-4">
+        <span className="w-full h-[2px] relative bg-black before:content-[''] before:absolute before:bottom-2 before:left-0 before:w-full before:h-[2px] before:bg-black after:content-[''] after:absolute after:top-2 after:left-0 after:w-full after:h-[2px] after:bg-black"></span>
+      </button>
+    </nav>
+  );
+}
