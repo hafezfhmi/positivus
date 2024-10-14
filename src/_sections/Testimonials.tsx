26914abada1295a9708c1034320eec8a120f48eb
hafezfhmi <hafezfahmi2000@gmail.com>
2024-10-15 03:32:49 +0800

    feat: add Testimonials section

---
+++ src/_sections/Testimonials.tsx
@@ -0,0 +1,64 @@
+import Arrow from "@/_components/Arrow";
+import Card from "@/_components/Card";
+
+type slideSelectorProps = {
+  className?: string;
+};
+
+function SlideSelector({ className }: slideSelectorProps) {
+  return (
+    <svg
+      width="14"
+      height="14"
+      viewBox="0 0 14 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path
+        d="M7.0099 2.05941L14 0L11.9604 7.0099L14 14L7.0099 11.9604L0 14L2.05941 7.0099L0 0L7.0099 2.05941Z"
+        fill="white"
+      />
+    </svg>
+  );
+}
+
+export default function Testimonials() {
+  return (
+    <section className="mt-16 px-5">
+      <div className="text-center">
+        <h2 className="px-[0.43rem] text-h2-mob font-medium bg-p-green inline rounded-md">
+          Testimonials
+        </h2>
+        <p className="mt-7 leading-6">
+          Hear from Our Satisfied Clients: Read Our Testimonials to Learn More
+          about Our Digital Marketing Services
+        </p>
+      </div>
+      <Card className="p-7 mt-10 bg-p-dark text-white">
+        <p className="relative border-[1px] border-p-green p-7 rounded-[2.8125rem] after:absolute after:left-12 after:bottom-0 after:translate-y-1/2 after:rotate-45 after:w-9 after:h-9 after:border-b-[1px] after:border-r-[1px] after:border-p-green after:bg-p-dark">
+          &quot;We have been working with Positivus for the past year and have
+          seen a significant increase in website traffic and leads as a result
+          of their efforts. The team is professional, responsive, and truly
+          cares about the success of our business. We highly recommend Positivus
+          to any company looking to grow their online presence.&quot;
+        </p>
+        <div className="ml-16 mt-10">
+          <p className="font-medium text-p-green text-lg">John Smith</p>
+          <p>Marketing Directory at XYZ Corp</p>
+        </div>
+        <div className="flex justify-between items-center mt-14 mb-7">
+          <Arrow className="rotate-180" />
+          <div className="flex gap-5">
+            <SlideSelector />
+            <SlideSelector />
+            <SlideSelector />
+            <SlideSelector />
+            <SlideSelector />
+          </div>
+          <Arrow />
+        </div>
+      </Card>
+    </section>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import LogoTicker from "@/_sections/LogoTicker";
 import Proposal from "@/_sections/Proposal";
 import Services from "@/_sections/Services";
 import Team from "@/_sections/Team";
+import Testimonials from "@/_sections/Testimonials";
 import WorkingProcess from "@/_sections/WorkingProcess";
 
 export default function Home() {
@@ -16,6 +17,7 @@ export default function Home() {
       <CaseStudies />
       <WorkingProcess />
       <Team />
+      <Testimonials />
     </main>
   );
 }
