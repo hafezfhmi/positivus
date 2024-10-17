388ff721c70e60186d07b1c1d96cba7c9f451367
hafezfhmi <hafezfahmi2000@gmail.com>
2024-10-18 02:45:55 +0800

    refactor: use tailwind sizing in Proposal

--- src/_sections/Proposal.tsx
+++ src/_sections/Proposal.tsx
@@ -6,11 +6,11 @@ export default function Proposal() {
     <section className="mt-16 px-5">
       <Card className="bg-p-grey" border={false}>
         <h3 className="font-medium text-h3-mob">Let’s make things happen</h3>
-        <p className="mt-[1.25rem] leading-6">
+        <p className="mt-5 leading-6">
           Contact us today to learn more about how our digital marketing
           services can help your business grow and succeed online.
         </p>
-        <Button className="mt-[1.25rem]">Get your proposal</Button>
+        <Button className="mt-5">Get your proposal</Button>
       </Card>
     </section>
   );
