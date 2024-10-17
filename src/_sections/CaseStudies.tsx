ebf3175c87c7553d06364f72856ff69e8dd6a81c
hafezfhmi <hafezfahmi2000@gmail.com>
2024-10-18 03:30:13 +0800

    refactor: use arrow component in CaseStudies; change sizing

--- src/_sections/CaseStudies.tsx
+++ src/_sections/CaseStudies.tsx
@@ -1,12 +1,12 @@
+import Arrow from "@/_components/Arrow";
 import Card from "@/_components/Card";
-import Image from "next/image";
 import Link from "next/link";
 
 export default function CaseStudies() {
   return (
     <section className="mt-16">
       <div className="text-center px-5">
-        <h2 className="px-[0.43rem] text-h2-mob font-medium bg-p-green inline rounded-md">
+        <h2 className="px-1.5 text-h2-mob font-medium bg-p-green inline rounded-md">
           Case Studies
         </h2>
         <p className="mt-7 leading-6">
@@ -23,10 +23,10 @@ export default function CaseStudies() {
           </p>
           <Link
             href="#"
-            className="mt-5 text-p-green flex items-center gap-[0.9375rem] text-xl"
+            className="mt-5 text-p-green flex items-center gap-3.5 text-xl"
           >
             Learn more
-            <Image src="/arrow.svg" alt="" width="17" height="10" />
+            <Arrow color1="fill-p-green" className="h-10 -rotate-[30deg]" />
           </Link>
         </Card>
         <Card className="flex-none basis-4/5 bg-p-dark text-white mt-10 py-10">
@@ -37,10 +37,10 @@ export default function CaseStudies() {
           </p>
           <Link
             href="#"
-            className="mt-5 text-p-green flex items-center gap-[0.9375rem] text-xl"
+            className="mt-5 text-p-green flex items-center gap-3.5 text-xl"
           >
             Learn more
-            <Image src="/arrow.svg" alt="" width="17" height="10" />
+            <Arrow color1="fill-p-green" className="h-10 -rotate-[30deg]" />
           </Link>
         </Card>
       </div>
