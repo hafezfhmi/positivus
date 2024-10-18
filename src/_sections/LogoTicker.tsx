3a1875995c5dd7f71476d38143135e307508ee91
hafezfhmi <hafezfahmi2000@gmail.com>
2024-10-18 15:53:55 +0800

    feat: add responsive LogoTicker

--- src/_sections/LogoTicker.tsx
+++ src/_sections/LogoTicker.tsx
@@ -3,11 +3,13 @@ import { Fragment } from "react";
 
 const logoGroup1 = ["amazon", "dribbble", "hubspot"];
 const logoGroup2 = ["netflix", "notion", "zoom"];
+const logoGroupCombined = [...logoGroup1, ...logoGroup2];
 
 export default function LogoTicker() {
   return (
     <section className="flex flex-col gap-5 mt-16 overflow-hidden">
-      <div className="flex gap-9 ">
+      {/* Mobile ticker start */}
+      <div className="flex md:hidden items-center gap-9">
         {[...new Array(2)].fill(0).map((_, index) => (
           <Fragment key={index}>
             {logoGroup1.map((logo) => (
@@ -17,13 +19,13 @@ export default function LogoTicker() {
                 key={logo}
                 width={124}
                 height={48}
-                className="w-[7.75rem] h-12 grayscale"
+                className="h-12 grayscale"
               />
             ))}
           </Fragment>
         ))}
       </div>
-      <div className="flex gap-9 ">
+      <div className="flex md:hidden items-center gap-9">
         {[...new Array(2)].fill(0).map((_, index) => (
           <Fragment key={index}>
             {logoGroup2.map((logo) => (
@@ -33,7 +35,25 @@ export default function LogoTicker() {
                 key={logo}
                 width={124}
                 height={48}
-                className="w-[7.75rem] h-12 grayscale"
+                className="h-12 grayscale"
+              />
+            ))}
+          </Fragment>
+        ))}
+      </div>
+      {/* Mobile ticker end */}
+      {/* Dekstop ticker start */}
+      <div className="hidden md:flex items-center gap-24">
+        {[...new Array(2)].fill(0).map((_, index) => (
+          <Fragment key={index}>
+            {logoGroupCombined.map((logo) => (
+              <Image
+                src={`/${logo}.svg`}
+                alt={logo}
+                key={logo}
+                width={124}
+                height={48}
+                className="h-12 grayscale"
               />
             ))}
           </Fragment>
