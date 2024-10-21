0051bf9dfd7b28014ee214ff6ac9c74d67b7c425
hafezfhmi <hafezfahmi2000@gmail.com>
2024-10-22 04:11:38 +0800

    fix: fix button hover adding extra height

--- src/_components/Button.tsx
+++ src/_components/Button.tsx
@@ -12,7 +12,7 @@ export default function Button({ className, href, children }: ButtonProps) {
     return (
       <Link
         href={href}
-        className={`inline-block w-full rounded-xl bg-p-dark px-7 py-5 text-center text-xl text-white ${className}`}
+        className={`inline-block w-full rounded-xl border bg-p-dark px-7 py-5 text-center text-xl text-white ${className}`}
       >
         {children}
       </Link>
@@ -20,7 +20,7 @@ export default function Button({ className, href, children }: ButtonProps) {
   }
   return (
     <button
-      className={`inline-block w-full rounded-xl bg-p-dark px-7 py-5 text-xl leading-7 text-white ${className}`}
+      className={`inline-block w-full rounded-xl border bg-p-dark px-7 py-5 text-xl leading-7 text-white ${className}`}
     >
       {children}
     </button>
